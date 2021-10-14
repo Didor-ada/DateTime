cf8e567c88984ced6466b8a4297ad6ef9917e1e2
Didor Ada <didor.dada@gmail.com>
2021-10-14 15:47:18 +0200

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace ProgrammeDateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = DateTime.Now;
+
+            // Console.WriteLine(date.Year);
+
+            CultureInfo cultureFrancais = CultureInfo.GetCultureInfo("fr-FR"); // sert à passer la langue en français après avoir installé le using, pour changer il faut par exemple passer le fr-FR en en-US pour de l'anglais
+
+            Console.WriteLine(date.ToString("dddd dd MMMM yyyy HH:mm:ss", cultureFrancais));
+
+            DateTime dateDemain = date.AddDays(1);
+
+            Console.WriteLine("Demain : " + dateDemain.ToString("ddd dd MMMM yyyy HH:mm:ss", cultureFrancais));
+
+            var diff = dateDemain - date;
+            Console.WriteLine("Différence heures : " + diff.TotalHours + "h");
+        }
+    }
+}
